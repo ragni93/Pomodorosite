09a9d9cb80b80207d7c738ce221cf708a986e2b7
ragni93 <ragni.eliassen@gmail.com>
2025-01-08 11:57:05 +0100

    test

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,23 +1,21 @@
-$(document).ready( function() { // function to start countdown 1!
-    $("#button").click(function () {
-        $(".boks1").load("countdown1.html", function () {
-            countDown1(); //function starts after countdown1.html is loaded
+
+//function to start Pomodoro-countdown
+$(document).ready( function() { //the code inside will run then the DOM is fully loaded
+    $("#button").click(function () { //function for what will happen when id #button is clicked
+        $(".boks1").load("countdown1.html", function () { //load countdown1.html in element with class boks1
+            countDown1(); //beginn this function after countdown1.html is loaded
         });
     })
 })
 
 
-
-
-
+//function for the countdown
 let countdownInterval;
 function countDown1(){
-    const intervall1= 0.1; //choose your starting time
-    let time = intervall1 * 60; // Transform to seconds.  10*60 = 600 sekunder. let fordi det skal forandres
-
-    const countdownEl = document.getElementById('countdown'); //plassere element i index
+    const intervall1= 7; //choose your starting time here, in minutes
+    let time = intervall1 * 60; // Transform the minutes to seconds, dont change.
+    const countdownEl = document.getElementById('countdown'); //place element in div-container
     clearInterval(countdownInterval); //clears any previous countdowns
-
     countdownInterval =setInterval(updateCountdown,1000); // run the updateCountdown-method every second!
 
 
@@ -32,8 +30,7 @@ function countDown1(){
 
         if(time < 0){
             clearInterval(countdownInterval); //tømmer id-en "countdownInterval", dvs stopper tellingen
-            $(".boks1").load("startpause1.html");
-
+            $(".boks1").load("startpause1.html"); //when time is below 0: starts the pause function into boks1 in index.html
         }
     }
 }
